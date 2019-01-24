c09e4c3e19204aa3ac0342b3cc64bbae4c6a2587
Glitch (hello-express) <none>
2019-01-24 01:55:28 +0000

    👝🌯 Checkpoint ./public/client.js:2729385/354

--- public/client.js
+++ public/client.js
@@ -249,7 +249,7 @@ masterGainController.addEventListener("input", function(evt) {
   masterGainNode.gain.value = this.value;
 });
 //EFFECTS
-//routing: sources -> gain node -> filter(optional) -> analyser -> convolver(optional) -> master gain -> output
+//routing: sources -> gain node -> filter1->filter2 (optional) -> analyser -> convolver(optional) -> master gain -> output
 var isReverbOn = false;
 var isFilterOn = false;
 
@@ -258,12 +258,13 @@ var toggleFilter = document.getElementById("toggle-filter");
 toggleFilter.addEventListener("click", function() {
   if(!isFilterOn) {
     isFilterOn = true; 
-    gainNode.connect(biquadFilter);
-    biquadFilter.connect(analyser);
+    gainNode.connect(biquadFilter1);
+    biquadFilter2.connect(analyser);
     gainNode.gain.value = 0.5;
   } else {
     isFilterOn = false;
-    biquadFilter.disconnect();
+    biquadFilter1.disconnect();
+    biquadFilter2.disconnect();
     gainNode.connect(analyser);
     if(isReverbOn) analyser.connect(convolver);
     else analyser.connect(masterGainNode);
@@ -273,25 +274,31 @@ toggleFilter.addEventListener("click", function() {
 });
 
 
-var biquadFilter = audioCtx.createBiquadFilter();
-biquadFilter.type = "lowpass";
-biquadFilter.Q.value = 1;
+var biquadFilter1 = audioCtx.createBiquadFilter();
+biquadFilter1.type = "lowpass";
+var biquadFilter2 = audioCtx.createBiquadFilter();
+biquadFilter2.type = "lowpass";
+biquadFilter1.connect(biquadFilter2);
+
 // biquadFilter.gain.setValueAtTime(25, audioCtx.currentTime);
 
 var inputFilterFreq = document.getElementById("filter-freq");
 inputFilterFreq.addEventListener('input', function(event){
-  biquadFilter.frequency.setValueAtTime(this.value, audioCtx.currentTime);
+  biquadFilter1.frequency.setValueAtTime(this.value, audioCtx.currentTime);
+  biquadFilter2.frequency.setValueAtTime(this.value, audioCtx.currentTime);
   console.log("freq:", this.value);
   
 });
 var inputFilterQ = document.getElementById("filter-Q");
 inputFilterQ.addEventListener('input', function(event){
-  biquadFilter.Q.value = this.value;
+  biquadFilter1.Q.value = this.value;
+  biquadFilter2.Q.value = this.value;
   console.log("Q:", this.value);
 });
 
 function filterChange(type) {
-  biquadFilter.type = type;
+  biquadFilter1.type = type;
+  biquadFilter2.type = type;
 }
 //reverb effect
 var convolver = audioCtx.createConvolver();
