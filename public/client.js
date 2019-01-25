d217a54ef46fcbf4fabc36c6841ec7b808cd6fde
Glitch (hello-express) <none>
2019-01-25 11:38:10 +0000

    📯👕 Checkpoint ./public/client.js:2729385/809

--- public/client.js
+++ public/client.js
@@ -383,16 +383,12 @@ function drawOscilloscope() {
   };
 
   draw();
-}
-hideAddressBar();
-function hideAddressBar(){
-  if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
-    document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
-  setTimeout(window.scrollTo(1,1),10);
-}
-window.addEventListener("load",function(){hideAddressBar();});
-window.addEventListener("orientationchange",function(){hideAddressBar();});
+  
+  window.addEventListener("orientationchange", function() {
+    document.body.requestFullscreen();
 
+  });
+}
 
 
 
