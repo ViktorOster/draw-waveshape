ccdf8658182d1132363d74ff14346090794b6c10
Glitch (hello-express) <none>
2019-01-25 11:27:12 +0000

    🌨🏏 Checkpoint ./views/index.html:2729385/429 ./public/style.css:2729385/29 ./public/client.js:2729385/407

--- public/client.js
+++ public/client.js
@@ -384,6 +384,14 @@ function drawOscilloscope() {
 
   draw();
 }
+hideAddressBar();
+function hideAddressBar(){
+  if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
+    document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
+  setTimeout(window.scrollTo(1,1),10);
+}
+window.addEventListener("load",function(){hideAddressBar();});
+window.addEventListener("orientationchange",function(){hideAddressBar();});
 
 
 
