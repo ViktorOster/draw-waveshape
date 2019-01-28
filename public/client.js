e8744357c874710785b4fd5cc1d4ee3a4c23be0a
Glitch (hello-express) <none>
2019-01-28 00:12:11 +0000

    🙉⏳ Checkpoint ./public/client.js:2729385/80 ./public/style.css:2729385/545

--- public/client.js
+++ public/client.js
@@ -15,9 +15,9 @@ var canvasGridColor = "#eee";
 // Create gradient
 var canvasGradient = canvas2Ctx.createRadialGradient(72.000, 71.100, 0.000, 123.000, 135.000, 150.000);
 // Add colors
-canvasGradient.addColorStop(0.000, 'rgba(255, 255, 255, 1.000)');
-canvasGradient.addColorStop(0.785, 'rgba(240, 240, 240, 1.000)');
-canvasGradient.addColorStop(1.000, 'rgba(235, 235, 235, 1.000)');
+canvasGradient.addColorStop(0.000, 'rgba(235, 235, 235, 1.000)');
+canvasGradient.addColorStop(0.785, 'rgba(233, 233, 230, 1.000)');
+canvasGradient.addColorStop(1.000, 'rgba(238, 238, 238, 1.000)');
 var canvasBackgroundColor = canvasGradient;
 
 var tone = 441;
